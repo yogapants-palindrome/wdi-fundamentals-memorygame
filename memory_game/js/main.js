a1a222f3a63b0d9e48d5a0e7aea9b1bc3adf06cc
Hannah Keith <hannahkeith@Hannahs-Air.attlocal.net>
2020-04-14 11:51:11 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardOne);
